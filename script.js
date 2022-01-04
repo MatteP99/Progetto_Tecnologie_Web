2cfcbc8334c7d3bf2f4ff2488a8193062a0fb34f
Matteo Paolucci <matteo.paolucci4@studio.unibo.it>
2022-01-04 19:08:00 +0100

    Update script.js
    Added functions to handle cookies

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
-    $.getJSON("dummy_page.php", function(data) {
+    setCookie("cart", '{"cart_ids":""}', 1);
+    /* $.getJSON("dummy_page.php", function(data) {
         let formattedData = formatData(data);
-        $("main").append(formattedData);    
+        $("main").append(formattedData);
+    }); */
+    $(":button").click(function(event) {
+        event.preventDefault();
+        addToCart($(this).val());
     });
 });
 
@@ -13,3 +18,36 @@ function formatData(data) {
     }
     return formattedData;
 }
+
+function addToCart(id) {
+    cart = getCookie("cart");
+    cartObj = JSON.parse(cart);
+    if (cartObj.cart_ids === "") {
+        cartObj.cart_ids = id;
+    } else {
+        cartObj.cart_ids += ',' + id;
+    }
+    setCookie("cart", JSON.stringify(cartObj), 1);
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
