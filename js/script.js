3d42ef3dcd39cb5b3f35eea7350491742024da58
Matteo Paolucci <matteo.paolucci4@studio.unibo.it>
2022-01-21 17:08:31 +0100

    Notifications Implemented
    Implemented dynamic notifications

--- js/script.js
+++ js/script.js
@@ -1,53 +1,99 @@
 $(document).ready(function () {
-    setCookie("cart", '{"cart_ids":""}', 1);
-    /* $.getJSON("dummy_page.php", function(data) {
-        let formattedData = formatData(data);
-        $("main").append(formattedData);
-    }); */
-    $(":button").click(function(event) {
-        event.preventDefault();
-        addToCart($(this).val());
-    });
+  setCookie("cart", '{"cart_ids":""}', 1);
+  /* $.getJSON("dummy_page.php", function(data) {
+      let formattedData = formatData(data);
+      $("main").append(formattedData);
+  }); */
+  $(":button").click(function(event) {
+    event.preventDefault();
+    addToCart($(this).val());
+  });
+  sendNotification("Prova", "Descrizione di prova");
+  setInterval(update_time, 1000 * 60);
 });
 
 function formatData(data) {
-    let formattedData = '';
-    for(let i = 0; i < data.length; i++) {
-        let dataItem = '';
-        formattedData.append(dataItem);
-    }
-    return formattedData;
+  let formattedData = '';
+  for(let i = 0; i < data.length; i++) {
+    let dataItem = '';
+    formattedData.append(dataItem);
+  }
+  return formattedData;
 }
 
 function addToCart(id) {
-    cart = getCookie("cart");
-    cartObj = JSON.parse(cart);
-    if (cartObj.cart_ids === "") {
-        cartObj.cart_ids = id;
-    } else {
-        cartObj.cart_ids += ',' + id;
-    }
-    setCookie("cart", JSON.stringify(cartObj), 1);
+  cart = getCookie("cart");
+  cartObj = JSON.parse(cart);
+  if (cartObj.cart_ids === "") {
+    cartObj.cart_ids = id;
+  } else {
+    cartObj.cart_ids += ',' + id;
+  }
+  setCookie("cart", JSON.stringify(cartObj), 1);
 }
 
 function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
+  return "";
+}
 
 function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function sendNotification(title, description) {
+  $("body > header").append(`
+  <div class="toast-container">
+    <span>${Date.now()}</span>
+    <div class="toast">
+      <div class="toast-header">
+        <strong class="mr-auto">${title}</strong>
+        <small>0 min ago</small>
+        <button type="button" class="close">
+          <span>&times;</span>
+        </button>
+      </div>
+      <div class="toast-body">
+        ${description}
+      </div>
+    </div>
+  </div>
+  `);
+  $(".toast-container:last").fadeIn();
+  $(".toast-header > button").click(function(event) {
+    event.preventDefault();
+    $(this).parents(".toast-container").fadeOut("fast", function() {
+      sleep(300);
+      $(this).remove();
+    });
+  });
+}
+
+function update_time() {
+    $(".toast-container").each( function() {
+      let start = new Date(parseInt($(this).find("span:first").text()));
+      let time = new Date(Date.now() - start);
+      $(this).find("small").text(time.getMinutes() + " min ago");
+    });
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
