e12735031b9d2208b7dac26032e96e2b333e545b
Matteo Paolucci <matteo.paolucci4@studio.unibo.it>
2022-01-26 14:56:10 +0100

    Merge completed

--- template/base.php
+++ template/base.php
@@ -1,20 +1,50 @@
 <!DOCTYPE html>
 <html lang="it">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title><?php echo $templateParams["title"]?></title>
-		<link rel="stylesheet" type="text/css" href="./css/style.css" />
-		<script
-			  src="https://code.jquery.com/jquery-3.6.0.js"
-			  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-			  crossorigin="anonymous"></script>
-		<?php?>
-	</head>
-	<body>
-		<header>
-			<!--Transizione di immagini + Titolo-->
-		</header>
+    <head>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href=
+            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" />
+        <script
+            src="https://code.jquery.com/jquery-3.6.0.js"
+            integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+            crossorigin="anonymous"></script>
+        <script src="js/script.js"></script>
+        <title><?php echo $templateParams["title"]?></title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <nav>
+            <div>
+                <a class="icon">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
+            <div class="nav_logo">
+                <img src="./upload/logo.png" alt="logo" />
+            </div>
+            <div class="nav_links">
+                <ul>
+                    <li><a <?php isActive("home.php");?> href="home.php">HOME</a></li>
+                    <li><a <?php isActive("menu.php");?> href="menu.php">MENU</a></li>
+                    <li><a <?php isActive("delivery.php");?> href="delivery.php">DELIVERY</a></li>
+                    <li><a <?php isActive("contacts.php");?> href="contacts.php">CONTATTI</a></li>
+                    <li><a <?php isActive("aboutus.php");?> href="aboutus.php">SU DI NOI</a></li>
+                </ul>
+            </div>
+            <div class="nav_contacts">
+                <ul>
+                    <li><a class="fab fa-facebook" href="https://www.facebook.com/Osteria-Zaccolla"></a></li>
+                    <li><a class="fab fa-instagram" href="https://www.instagram.com/Osteria-Zaccolla"></a></li>
+                    <li><a class="fab fa-twitter" href="https://www.twitter.com/Osteria-Zaccolla"></a></li>
+                </ul>
+                <p>Piazza Baschetti 23, 47863, Cesena (FC)</p>
+            </div>
+        </nav>
+        <header>
+            <section class="notifications"></section>
+            <img src="upload/slideshow/img1.jpg" alt=""/>
+        </header>
 		<main>
 		<?php
 		if(isset($templateParams["name"])){
@@ -22,22 +52,21 @@
 		}
 		?>
 		</main>
-		<aside>
-			<nav>
-				<ul>
-					<!--Login-->
-					<li><a <?php isActive("home.php");?> href="index.php">HOME</a></li>
-					<li><a <?php isActive("menu.php");?> href="menu.php">MENU</a></li> <!--MENU RISTORANTE-->
-					<li><a <?php isActive("delivery.php");?> href="delivery.php">DELIVERY</a></li>
-					<li><a <?php isActive("contacts.php");?> href="contacts.php">CONTATTI</a></li>
-					<li><a <?php isActive("aboutus.php");?> href="aboutus.php">SU DI NOI</a></li>
-					<!--Collegamento Facebook-->
-					<!--Collegamento Instagram -->
-				</ul>
-			</nav>
-		</aside>
-		<footer>
-			<p>FOOTER</p> <!--Immagini ristorante/premi + numero di tel, e-mail, via ecc.-->
-		</footer>
-	</body>
+        <footer>
+            <div>
+                <img src="./upload/logo.png" alt="logo" />
+            </div>
+            <div>
+                <div>                
+                    <ul>
+                        <li><a class="fas fa-phone" href="tel:3317848795"> 331-7848795</a><li>
+                        <li><a class="fas fa-pen" href="mailto:info@osteriazaccolla.it"> info@osteriazaccolla.it</a><li>
+                    </ul>
+                </div>
+                <div>
+                    <p>Piazza Baschetti 23, 47863, Cesena (FC)</p>
+                </div>
+            </div>
+        </footer>
+    </body>
 </html>
