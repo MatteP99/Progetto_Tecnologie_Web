f986188e7fcb4d9f1e61f33ecdb32ef138e421d7
Zacandri <andrea.zacconi2@studio.unibo.it>
2022-02-11 20:39:41 +0100

    Fixxato il pagamento

--- payment.php
+++ payment.php
@@ -5,43 +5,64 @@ if(isset($_POST["submit"])) {
 	$db->createOrder($_SESSION["id_user"]);
 	$lastOrder = $db->getIdOrder();
 	$lastOrder = $lastOrder[0]["MAX(orders.id_order)"];
-	$cookie = str_replace(array('"', 'c', 'a', 'r', 't', '_', 'i', 'd', 's', ':', '{', '}'), '', (string)$_COOKIE["cart"]);
+	$cookie = str_replace(array('"', 'c', 'a', 'r', 't', '_', 'i', 'd', 's', '{', '}'), '', (string)$_COOKIE['cart']);
 	$arrayFood = array();
 	$arrayQuantity = array();
+	$arrayTemp = array();
 	for($i = 0; $i < strlen($cookie); $i++){
-		if($cookie[$i] == ",") {
-			array_push($arrayFood, (int)$cookie[$i - 2]);
-			array_push($arrayQuantity, (int)$cookie[$i - 1]);
+		if($i != 0 && $cookie[$i] != ":" && $cookie[$i] != ",") {
+			array_push($arrayTemp, $cookie[$i]);
+		}
+		if($cookie[$i] == ":" && $i != 0) {
+			$string = "";
+			for($j = 0; $j < count($arrayTemp); $j++) {
+				$string = $string.$arrayTemp[$j];
+			}
+			array_push($arrayFood, (int)$string);
+			unset($arrayTemp);
+			$arrayTemp = array();
+		} else if($cookie[$i] == ",") {
+			$string = "";
+			for($j = 0; $j < count($arrayTemp); $j++) {
+				$string = $string.$arrayTemp[$j];
+			}
+			array_push($arrayQuantity, (int)$string);
+			unset($arrayTemp);
+			$arrayTemp = array();
 		} else if($i == strlen($cookie) - 1) {
-			array_push($arrayFood, (int)$cookie[$i - 1]);
-			array_push($arrayQuantity, (int)$cookie[$i]);
+			$string = "";
+			for($j = 0; $j < count($arrayTemp); $j++) {
+				$string = $string.$arrayTemp[$j];
+			}
+			array_push($arrayQuantity, (int)$string);
 		}
 	}
 	$totalPrice = 0.0;
+	print_r($arrayFood);
+	print_r($arrayQuantity);
 	for($i = 0; $i < count($arrayFood); $i++) {
 		$price = $db->getFoodPrice($arrayFood[$i]);
 		$totalPrice = $totalPrice + ((double)$price[0]["price"] * $arrayQuantity[$i]);
-		
 		$quantity_food = $db->getFoodQuantity($arrayFood[$i]);
-		$final_quantity = $quantity_food[0]["quantity"] - $arrayQuantity[$i];
+		$final_quantity = (double)$quantity_food[0]["quantity"] - $arrayQuantity[$i];
 		
 		$db->removeQuantityFood($arrayFood[$i], $final_quantity);
 		$db->insertListFood($lastOrder, $arrayFood[$i], $arrayQuantity[$i]);
 		$check_food = $db->getFoodQuantity($arrayFood[$i]);
-		if($check_food[0]["quantity"] == 0) {
+		if($check_food == 0) {
 			$db->createNotifyAdminQuantityFood($arrayFood[$i]);
-		} else if ($check_food[0]["quantity"] <= 5) {
+		} else if ($check_food <= 5) {
 			$db->createNotifyAdminLowQuantityFood($arrayFood[$i]);
 		}
-		$db->createNotifyAdminOrder($lastOrder);
 	}
+	$db->createNotifyAdminOrder($lastOrder);
 	if($_SESSION["user_status"] == "Cliente-Studente") {
 		$totalPrice = $totalPrice - ($totalPrice * 0.2);
 	}
 	$db->insertTotalPrice($lastOrder, $totalPrice);
     unset($_COOKIE['cart']); 
     setcookie('cart', null, -1, '/');
-    header("location:login.php");
+	header("location:login.php");
 }
 //Check if user is logged
 if(isUserLoggedIn()){
